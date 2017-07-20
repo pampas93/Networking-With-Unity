b3dc8c9b4c34902fce189f39e6f05ee4c7f162cc
pampas93 <abhijit.93@hotmail.com>
2017-07-20 12:25:39 -0400

    Color change onClick using rayCasting

--- Assets/SelectionWithRayCast.cs
+++ Assets/SelectionWithRayCast.cs
@@ -7,6 +7,25 @@ public class SelectionWithRayCast : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Button clicked");
+            RaycastHit hit;
+
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);  // Construct a ray from the current mouse coordinates
+            if (Physics.Raycast(r, out hit))
+            {
+                if (hit.collider.gameObject != null)
+                {
+                    GameObject obj = hit.collider.gameObject;
+                    Color x = obj.GetComponent<Renderer>().material.color;
+                    if (x != Color.red)
+                        obj.GetComponent<Renderer>().material.color = Color.red;
+                    else
+                        obj.GetComponent<Renderer>().material.color = Color.blue;
+                }
+            }
+        }
+    }
 }
